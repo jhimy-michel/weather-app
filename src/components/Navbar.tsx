97a4b604485bdd4a1f8b5c2796ecda095285ea20
jhimy-michel <jhimy0riv@gmail.com>
2024-03-18 09:08:14 +0100

    adding basic layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { github } from "@/utils/icons";
+
+export default function Navbar() {
+  const router = useRouter();
+
+  return (
+    <div className="w-full py-4 flex items-center justify-between">
+      <div className="left"></div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <Button
+          className="source-code-btn flex items-center gap-2"
+          onClick={() => {
+            router.push("https://github.com");
+          }}
+        >
+          {github} Search
+        </Button>
+      </div>
+    </div>
+  );
+}
