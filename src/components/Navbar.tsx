f36e7f8b1e0cc30e9045613950f21de3e75187c6
jhimy-michel <jhimy0riv@gmail.com>
2024-03-18 09:17:12 +0100

    adding them dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { github } from "@/utils/icons";
+import { ThemeDropdown } from "./ThemeDropdown/ThemeDropdown";
 
 export default function Navbar() {
   const router = useRouter();
@@ -11,6 +12,7 @@ export default function Navbar() {
     <div className="w-full py-4 flex items-center justify-between">
       <div className="left"></div>
       <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <ThemeDropdown />
         <Button
           className="source-code-btn flex items-center gap-2"
           onClick={() => {
